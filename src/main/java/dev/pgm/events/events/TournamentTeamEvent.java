ef166a6a36372ab6cae7ca8ccc3cbd2eca282543
Pugzy <pugzy@mail.com>
2020-12-28 15:36:51 +0000

    Add handlers for TournamentTeamEvent class

--- src/main/java/dev/pgm/events/events/TournamentTeamEvent.java
+++ src/main/java/dev/pgm/events/events/TournamentTeamEvent.java
@@ -2,12 +2,13 @@ package dev.pgm.events.events;
 
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 import tc.oc.pgm.api.party.Party;
 
 public class TournamentTeamEvent extends Event implements Cancellable {
 
+    private static final HandlerList handlers = new HandlerList();
     private Party team;
-
     private boolean cancelled = false;
 
     public TournamentTeamEvent(Party team) {
@@ -26,5 +27,14 @@ public class TournamentTeamEvent extends Event implements Cancellable {
     @Override
     public void setCancelled(boolean cancelled) {
         this.cancelled = cancelled;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
     }
 }
